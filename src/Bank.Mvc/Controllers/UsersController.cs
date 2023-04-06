1927b31da26668c365ead5841f8d7b16ef64de16
MuzaffarNurillaew <nurillaewmuzaffar@gmail.com>
2023-04-06 11:07:12 +0500

    Users controller added

---
+++ src/Bank.Mvc/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Bank.Service.Dtos.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bank.Mvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index", new UserDto()
+            {
+                FirstName = "Muzaffar",
+                LastName = "Nurillayev",
+                Balance = 0,
+                DateOfBirth = DateTime.Now,
+                Email = "nurillaewmuzaffar@gmail.com",
+                Phone = "324234"
+            });
+        }
+    }
+}
