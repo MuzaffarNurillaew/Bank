169b0ad457b4f282d2dd1ce70983316cd3706540
MuzaffarNurillaew <nurillaewmuzaffar@gmail.com>
2023-04-06 09:25:09 +0500

    Revert "AutoMapper added"
    This reverts commit 51d29550a5a54fc3301dd3bd72228cf338058f20.

--- src/Bank.Service/Interfaces/IUserService.cs
+++ src/Bank.Service/Interfaces/IUserService.cs
@@ -1,5 +1,4 @@
-﻿using Bank.Domain.Configuration;
-using Bank.Domain.Entities;
+﻿using Bank.Domain.Entities;
 using Bank.Service.Dtos.Users;
 using System.Linq.Expressions;
 
@@ -8,9 +7,8 @@ namespace Bank.Service.Interfaces
     public interface IUserService
     {
         Task<UserDto> CreateAsync(UserCreationDto userDto);
-        Task<UserDto> UpdateAsync(Expression<Func<User, bool>> expression, UserCreationDto userDto);
+        Task<UserDto> UpdateAsync(UserCreationDto userDto);
         Task<bool> DeleteAsync(Expression<Func<User, bool>> expression);
         Task<UserDto> GetAsync(Expression<Func<User, bool>> expression);
-        Task<IEnumerable<UserDto>> GetAllAsync(PaginationParams @params, Expression<Func<User, bool>> expression = null);
     }
 }
